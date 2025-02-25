7981566d883c090c22e5ccc5d83e3786db6fbf1c
akbartaimur <134905706+akbartaimurr@users.noreply.github.com>
2025-02-25 20:50:22 +0400

    trying to fix sum

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ async def generate_transcript(request: TranscriptRequest):
     try:
         # Prepare Actor input
         run_input = {
-            "outputFormat": "textWithTimestamps",
+            "outputFormat": "captions",
             "urls": [request.url],
             "maxRetries": 6,
             "proxyOptions": {
